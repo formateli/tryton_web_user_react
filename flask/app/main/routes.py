c89201b1bd42f42707da6061a0fdce890d1926cd
formateli <framirez@formateli.com>
2025-06-30 17:14:12 +0000

    wip

--- flask/app/main/routes.py
+++ flask/app/main/routes.py
@@ -2,29 +2,9 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 import functools
-from flask import request, current_app, json, Response
+from flask import request, Response
 from app import tryton, wu
 from app.main import bp
-from trytond.res.user import PasswordError
-from trytond.modules.web_user.exceptions import UserValidationError
-
-#def tryton_route(func):
-#    @functools.wraps(func)
-#    def wrapper(*args, **kwargs):
-#        args_repr = [repr(a) for a in args]
-#        kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
-#        signature = ", ".join(args_repr + kwargs_repr)
-#        #raise Exception('Database name: ' + signature)
-#        #return func(*args, **kwargs)
-#        if request.method == 'OPTIONS':
-#            return Response('preflight ok', 200, {
-#                'Access-Control-Allow-Origin':'*',
-#                'Access-Control-Allow-Methods':'GET,POST,PUT,DELETE',
-#                'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-#                })
-#        #raise Exception('METHOD: ' + request.method)
-#        return func()
-#    return wrapper
 
 
 @bp.route('/<database_name>/web-user-register', methods=['OPTIONS', 'POST'])
@@ -34,11 +14,11 @@ def web_user_register():
     User = tryton.pool.get('res.user')
     WebUser = tryton.pool.get('web.user')
     args = request.get_json(False)
-    current_app.logger.info(str(args))
     try:
         user = WebUser.search([('email', '=', args['username'])])
         if user:
-            return Response('User already exists.', 403)
+            return wu.response_exception(
+                    'User {} already exists.'.format(args['username']), 403)
         user = WebUser.create_web_user(tryton.pool, args)
         User.validate_password(args['password'], [user])
         user.save()
@@ -46,11 +26,10 @@ def web_user_register():
 
         #TODO Send confirmation email
 
-    except (PasswordError, UserValidationError) as e:
-        return Response(e, 403)
+    except (wu.WuPasswordError, wu.WuUserValidationError) as e:
+        return wu.response_exception(e, 403)
     except Exception as e:
-        current_app.logger.info(str(e))
-        return Response(e, 500)
+        return wu.response_exception(e, 500)
 
 
 @bp.route('/<database_name>/web-user-tokens',
@@ -65,16 +44,14 @@ def web_user_token():
 
     try:
         if request.method == 'DELETE':
-            #logger.info('DELETE %s', auth.token)
             UserSession.remove(auth.token)
             return Response(None, 204)
 
         if request.method == 'POST':
             user = WebUser.authenticate(auth['username'], auth['password'])
             if user is None:
-                #logger.info('POST not found %s', auth['username'])
-                return Response('Not found.', 401)
-            #logger.info("POST user found: %s", user.email)
+                return wu.response_exception(
+                        'Not found. {}.'.format(auth['username']), 401)
             key = user.new_session()
             return {'access_token': key}
 
@@ -84,22 +61,22 @@ def web_user_token():
             if sessions:
                 session = sessions[0]
             if session is None:
-                return Response('Session not found.', 404)
+                return wu.response_exception('Session not found.', 404)
             key = session.key
             if session.expired:
-                #logger.info('PUT session expired %s', key)
                 if not user.stay_logged_in:
-                    return Response('Session expired.', 401)
+                    return wu.response_exception(
+                        'Session expired. {}.'.format(session.user.email), 401)
                 user = session.user
                 UserSession.remove(session.key)
                 key = user.new_session()
-                #logger.info('PUT renew session %s', key)
             return {'access_token': key}
 
-        return Response('Invalid request method.', 405)
+        return wu.response_exception(
+                'Invalid request method. {}'.format(request.method), 405)
 
     except Exception as e:
-        return Response(e, 500)
+        return wu.response_exception(e, 500)
 
 
 @bp.route('/<database_name>/web-user-me', methods=['OPTIONS', 'GET'])
@@ -111,11 +88,7 @@ def web_user_me():
     try:
         user = WebUser.get_user(auth.token)
         if user is None:
-            return Response('Invalid', 401)
-        current_app.logger.info(user.to_json())
+            return wu.response_exception('Invalid user token', 401)
         return user.to_json()
     except Exception as e:
-        current_app.logger.info(str(e))
-        return Response(str(e), 500)
-
-    #return Response('ok', 200, {'Access-Control-Allow-Origin':'*'})
+        return wu.response_exception(e, 500)
--- flask/app/web_user.py
+++ flask/app/web_user.py
@@ -3,11 +3,17 @@
 # the full copyright notices and license terms.
 from functools import wraps
 from trytond.config import config
+from trytond.res.user import PasswordError
+from trytond.modules.web_user.exceptions import UserValidationError
 from flask import current_app, request, Response, json
 
 __all__ = ["WebUser", "wu_route"]
 
 class WebUser:
+
+    WuPasswordError = PasswordError
+    WuUserValidationError = UserValidationError
+
     def __init__(self):
         self.cors = []
         self.timeout = None
@@ -27,32 +33,37 @@ class WebUser:
             def wrapper(*args, **kwargs):
                 wu = current_app.extensions['WebUser']
 
-                current_app.logger.info('ORIGIN: ' + request.origin)
-                current_app.logger.info('METHOD: ' + request.method)
-
                 headers = {}
                 if request.origin in wu.cors:
                     headers['Access-Control-Allow-Origin'] = request.origin
-                    #headers['Access-Control-Max-Age'] = wu.timeout
+                    headers['Vary'] = 'Origin'
                 current_app.logger.info(str(headers))
                 if request.method == 'OPTIONS':
-                    #headers['Vary'] = 'Origin'
-                    #method = request.headers.get('Access-Control-Request-Method')
-                    #if method:
-                    #    headers['Access-Control-Allow-Methods'] = method
                     headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
                     headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE'
-                    #headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorizatio'
-                    current_app.logger.info('PREFLIGHT HEADERS: ' + str(headers))
+                    headers['Access-Control-Max-Age'] = wu.timeout
                     return Response('preflight ok', 200, headers)
                 res = func()
                 if isinstance(res, dict):
                     res = Response(json.dumps(res), 200)
                 if isinstance(res, Response):
                     res.headers = headers
-                    current_app.logger.info('RES HEADERS: ' + str(res.headers))
                 return res
             return wrapper
         return decorator
+
+    @staticmethod
+    def response_exception(e, status):
+        if hasattr(e, 'message'):
+            message = e.message
+        else:
+            message = str(e)
+
+        if status >= 500:
+            current_app.logger.error(message)
+        else:
+            current_app.logger.warning(message)
+
+        return Response(message, status)
 
 wu_route = WebUser.route
